9e2f754b479f74ebc08ae9d5b337ccc2bd22f591
Michel <106674127+MichelZero@users.noreply.github.com>
2022-06-23 20:55:39 -0300

    Create ex02.py

---
+++ ex02.py
@@ -0,0 +1,16 @@
+#
+# 	Implemente um programa em linguagem Python que recebe como entradas a base e a altura de um triângulo
+# 	e apresente ao usuário a área deste triângulo. A área de um triângulo é calculada a partir da
+# 	seguinte expressão: A_T=(b*h)/2, em que b é a base do triângulo e h a sua altura.
+#
+# 	entrada de dados
+base = float(input("informe a base do triângulo: ")) # solicita a base do triângulo
+altura = float(input("informe a altura do triângulo: ")) # solicita a altura do triângulo
+
+# processamento
+area = (base * altura) / 2
+
+# saída de dados
+print(f"A área do triângulo é: {area}")
+
+print("Fim do programa")
