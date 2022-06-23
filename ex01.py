ffc38184ddff133f3da8515bd3f0e3275fa0675a
Michel <106674127+MichelZero@users.noreply.github.com>
2022-06-23 20:52:26 -0300

    Update ex01.py

---
+++ ex01.py
@@ -0,0 +1,18 @@
+#
+# 1.	De uma forma geral, pode-se afirmar que os Algoritmos são compostos por um conjunto de entradas de
+# dados e uma etapa de processamento para a obtenção de sua(s) saída(s). Muitas vezes, as saídas de um
+# algoritmo são apresentadas em tela. Sabendo disso, desenvolva um programa em linguagem Python que
+# solicite do usuário o seu nome e apresente em tela um cabeçalho padrão, seguido pelo nome informado,
+# exatamente como ilustrado a seguir.
+#
+# entrada de dados
+nome = input("informe o seu nome: ") # solicita o nome do usuário
+
+# processamento
+print('**************************************')
+print('* ALGORTIMOS E LÓGICA DE PROGRAMAÇÃO *')
+print('* Turma: 1° INTIN   Ano letivo: 2022 *')
+print('* Professores: Leandro e Michel      *')
+print('* Monitores: Emanuel e Carlos        *')
+print('**************************************')
+print(f"Aluno: {nome}")
