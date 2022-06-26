7275b48d90aa60f3ba215df8301620e73c9b40f3
Carlos Eduardo ✔ <97265556+SmokeDevL@users.noreply.github.com>
2022-06-25 21:10:47 -0300

    Resolvendo Expressões

--- ex03.py
+++ ex03.py
@@ -1,8 +1,10 @@
+# Autores: Carlos, 
 #
-# 	Em linguagem Python, crie um programa que recebe como entrada do usuário o valor de uma variável x e seja
-# 	capaz de apresentar em tela o resultado da seguinte expressão matemática:
-#  E = ((x^2-x+1)/((x-5)/2))
+# Em linguagem Python, crie um programa que recebe como entrada do usuário o valor de uma variável x e seja
+# capaz de apresentar em tela o resultado da seguinte expressão matemática:
+# E = ((x^2-x+1)/((x-5)/2))
 #
+
 # entrada de dados
 x = float(input("informe o valor de x: ")) # solicita o valor de x
 
