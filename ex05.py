6be934a9547094ba59d7636b6df3df77cd6b39b7
Carlos Eduardo ✔ <97265556+SmokeDevL@users.noreply.github.com>
2022-06-25 21:21:47 -0300

    Porcentagem

--- ex05.py
+++ ex05.py
@@ -1,11 +1,12 @@
+# Autores: Carlos, 
 #
 # 5.	Desenvolva um programa em linguagem Python que recebe do usuário o preço de um produto e um valor de desconto.
 # Com estas informações, o programa deve exibir o preço do produto com desconto. Observações: O valor do desconto
 # deve ser informado entre 1 (100%) e 0 (0%). Assim, 0.5 corresponde a um desconto de 50%. Neste momento,
 # considere que sempre serão informados valores válidos pelo usuário.
 # caso precise formatar o valor de um número numérico,
-# sugerimos que assistam a este vídeo: link para vídeo do Youtube.  https://youtu.be/7QuriwzICaY
-#
+# sugerimos que assistam a este vídeo: link para vídeo do Youtube. 'https://youtu.be/7QuriwzICaY'
+
 # entrada de dados
 preco = float(input("informe o preço do produto: ")) # solicita o preço do produto
 desconto = float(input("informe o valor do desconto: ")) # solicita o valor do desconto
