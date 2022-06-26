4f011eee33b061ffb37e85547536ffb1ee87ca6e
Carlos Eduardo ✔ <97265556+SmokeDevL@users.noreply.github.com>
2022-06-25 21:27:21 -0300

    Solicitando nome usuário

--- ex01.py
+++ ex01.py
@@ -1,10 +1,11 @@
+# Autores: Carlos, 
 #
 # 1.	De uma forma geral, pode-se afirmar que os Algoritmos são compostos por um conjunto de entradas de
 # dados e uma etapa de processamento para a obtenção de sua(s) saída(s). Muitas vezes, as saídas de um
 # algoritmo são apresentadas em tela. Sabendo disso, desenvolva um programa em linguagem Python que
 # solicite do usuário o seu nome e apresente em tela um cabeçalho padrão, seguido pelo nome informado,
 # exatamente como ilustrado a seguir.
-#
+
 # entrada de dados
 nome = input("informe o seu nome: ") # solicita o nome do usuário
 
