a358485bbf88fd7a25004bdb8b4cc9b503d4f791
Carlos Eduardo ✔ <97265556+SmokeDevL@users.noreply.github.com>
2022-06-28 00:21:44 -0300

    Polinômios ➗

--- ex04.py
+++ ex04.py
@@ -4,7 +4,7 @@
 # Emanuel Franklyn
 # Carlos Eduardo
 #
-# data: 26/06/2022
+# data: 27/06/2022
 #
 # 4. Elabore um programa em linguagem Python que implementa o
 # cálculo do valor de um polinômio do terceiro grau.         
@@ -13,16 +13,19 @@
 
 
 # Entrada de dados
-a = float(input("informe o valor de a: ")) # solicita o valor de a
-b = float(input("informe o valor de b: ")) # solicita o valor de b
-c = float(input("informe o valor de c: ")) # solicita o valor de c
-d = float(input("informe o valor de d: ")) # solicita o valor de d
-x = float(input("informe o valor de x: ")) # solicita o valor de x
+
+a = float(input("informe o valor de a: ")) # Solicita o valor de a
+b = float(input("informe o valor de b: ")) # Solicita o valor de b
+c = float(input("informe o valor de c: ")) # Solicita o valor de c
+d = float(input("informe o valor de d: ")) # Solicita o valor de d
+x = float(input("informe o valor de x: ")) # Solicita o valor de x
 
 # Processamento de dados
+
 p = (a*x**3) + (b*x**2) + (c*x) + d # Calcula o resultado do polinômio
 
 # Saída de dados
+
 print(f"\nO resultado da expressão é: {p}") # Imprimindo o resultado do polinômio
 
 print("fim do programa") # Informando ao usuário que o programa terminou
