bc03abf3d34fff6d8b828da9e57087d53e135bd5
Carlos Eduardo ✔ <97265556+SmokeDevL@users.noreply.github.com>
2022-06-28 00:18:46 -0300

    Calculando Expressões ➗

--- ex03.py
+++ ex03.py
@@ -4,7 +4,7 @@
 # Emanuel Franklyn
 # Carlos Eduardo
 #
-# data: 26/06/2022
+# data: 27/06/2022
 #
 # 3. Em linguagem Python, crie um programa que recebe como   
 # entrada do usuário o valor de uma variável x e seja capaz  
@@ -13,12 +13,15 @@
 
 
 # Entrada de dados
-x = float(input("informe o valor de x: ")) # solicita o valor de x
+
+x = float(input("informe o valor de x: ")) # Solicita o valor de x
 
 # Processamento de dados
-E = ((x**2-x+1)/((x-5)/2)) # Calcula a expressão
+
+E = ((x**2-x+1)/((x-5)/2)) # Calcula a expressão matemática
 
 # Saída de dados
+
 print(f"O resultado da expressão é: {E}") # Imprimindo o resultado da expressão
 
 print("fim do programa") # Informamos ao usuário que o programa terminou
